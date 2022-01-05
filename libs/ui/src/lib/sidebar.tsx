d846f20518edce3288c5c72aeba5e2d553675f0c
p6l-richard <richard.poelderl@gmail.com>
2022-01-05 20:21:49 +0100

    add shallow sidebar component

---
+++ libs/ui/src/lib/sidebar.tsx
@@ -0,0 +1,14 @@
+import './ui.module.css';
+
+/* eslint-disable-next-line */
+export interface SidebarProps {}
+
+export function Sidebar(props: SidebarProps) {
+  return (
+    <div>
+      <h1>Welcome to Sidebar!</h1>
+    </div>
+  );
+}
+
+export default Sidebar;
