c364e3b75de8696d876140f95446b55bb1a02a49
usama <usama7x@gmail.com>
2022-01-07 21:57:50 +0500

    refactor(sidebar): refactor and align code in sidebar component

--- libs/ui/src/lib/sidebar.tsx
+++ libs/ui/src/lib/sidebar.tsx
@@ -42,52 +42,54 @@ export function Sidebar(props: SidebarProps) {
   function onCheckboxChange(e: any) {
     console.log(`checked = ${e.target.checked}`);
   }
+
+  const sidebarContent = 
+    <div>
+      <Select
+        showSearch
+        placeholder="Duration"
+        optionFilterProp="children"
+        onChange={onChange}
+        onSearch={onSearch}
+      >
+        <Option value="30">30 Minutes</Option>
+        <Option value="lucy">45 Minutes</Option>
+        <Option value="tom">60 Minutes</Option>
+      </Select>
+      <br></br>
+      <br></br>
+      <Select
+        showSearch
+        placeholder="Event Type"
+        optionFilterProp="children"
+        onChange={onChange}
+        onSearch={onSearch}
+      >
+        <Option value="share">Share Slots</Option>
+        <Option value="individual">Individual Slot</Option>
+      </Select>
+      <br></br>
+      <br></br>
+      <Checkbox onChange={onCheckboxChange}>Save placeholders</Checkbox>
+      <br></br>
+      <TextArea rows={2} placeholder="Type placeholder name here"></TextArea>
+      <br></br>
+      <br></br>
+      <TextArea rows={5} onChange={onCopyChange} value={copyState.value} placeholder="Start selecting availabilities you would like to share on your calender"></TextArea>
+      <br></br>
+      <br></br>
+
+      <CopyToClipboard text={copyState.value}
+        onCopy={() => setCopyState({...copyState, copied: true})}>
+      <Button type="primary" icon={<DownloadOutlined />} shape="round" size={"large"}>
+        Copy to Clipboard
+      </Button>
+      </CopyToClipboard>
+    </div>
   
   return (
       <App_Sidebar
-      sidebar={<div>
-        <Select
-    showSearch
-    placeholder="Duration"
-    optionFilterProp="children"
-    onChange={onChange}
-    onSearch={onSearch}
-  >
-    <Option value="30">30 Minutes</Option>
-    <Option value="lucy">45 Minutes</Option>
-    <Option value="tom">60 Minutes</Option>
-  </Select>
-  <br></br>
-  <br></br>
-  <Select
-    showSearch
-    placeholder="Event Type"
-    optionFilterProp="children"
-    onChange={onChange}
-    onSearch={onSearch}
-  >
-    <Option value="share">Share Slots</Option>
-    <Option value="individual">Individual Slot</Option>
-  </Select>
-  <br></br>
-  <br></br>
-  <Checkbox onChange={onCheckboxChange}>Save placeholders</Checkbox>
-  <br></br>
-  <TextArea rows={2} placeholder="Type placeholder name here"></TextArea>
-  <br></br>
-  <br></br>
-<TextArea rows={5} onChange={onCopyChange} value={copyState.value} placeholder="Start selecting availabilities you would like to share on your calender"></TextArea>
-<br></br>
-<br></br>
-
-        <CopyToClipboard text={copyState.value}
-          onCopy={() => setCopyState({...copyState, copied: true})}>
-         <Button type="primary" icon={<DownloadOutlined />} shape="round" size={"large"}>
-          Copy to Clipboard
-        </Button>
-        </CopyToClipboard>
-      </div>
-    }
+      sidebar={sidebarContent}
       open={sidebarOpen}
       onSetOpen={onSetSidebarOpen}
       styles={{ sidebar: { background: "grey", width: "300px" } }}
